4b92dd29aa022e1937315ea83e1149bcd54610f7
ibrahem <ibrahem.al-radaei@outlook.com>
2024-12-06 02:47:42 +0300

    1

--- vendor_load.php
+++ vendor_load.php
@@ -10,6 +10,6 @@ if (isset($_REQUEST['test'])) {
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
 };
-require __DIR__ . '/vendor/autoload.php'; // TD
+require __DIR__ . '/../Translation_Dashboard/vendor/autoload.php'; // TD
 
 // require(__DIR__ . '/../../vendor/autoload.php');
