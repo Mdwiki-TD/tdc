9ba9e98b31e916c4a990dc9f5fe8b1c1b83e0123
ibrahem <ibrahem.al-radaei@outlook.com>
2025-04-23 02:25:18 +0300

    1

--- csrf.php
+++ csrf.php
@@ -9,13 +9,23 @@ use function TDWIKI\csrf\generate_csrf_token;
 use function TDWIKI\csrf\verify_csrf_token; // if (verify_csrf_token())  {
 */
 
-include_once __DIR__ . '/auth/helps.php';
-
-use function OAuth\Helps\decode_value;
-
 function verify_csrf_token()
 {
-	global $csrf_token_orginal;
+	// return true;
+	// ---
+	// بدء الجلسة إذا لم تكن مفتوحة بالفعل
+	// if (session_status() === PHP_SESSION_NONE) {
+	// 	session_name("mdwikitoolforgeoauth");
+	// 	session_start();
+	// }
+
+	// التحقق مما إذا كان هناك CSRF Tokens في الجلسة
+	if (!isset($_SESSION['csrf_tokens']) || !is_array($_SESSION['csrf_tokens'])) {
+		$_SESSION['csrf_tokens'] = [];
+		echo "No csrf tokens in session!";
+		return true;
+	}
+
 	// التحقق من وجود Token في الطلب
 	$submitted_token = $_POST['csrf_token'] ?? null;
 
@@ -24,12 +34,19 @@ function verify_csrf_token()
 		echo "No CSRF Token Submitted!";
 		return false;
 	}
-	$decoded_token = decode_value($submitted_token);
 
 	// التحقق مما إذا كان Token موجودًا في القائمة
-	if ($csrf_token_orginal == $decoded_token) {
+	if (in_array($submitted_token, $_SESSION['csrf_tokens'], true)) {
+		// Token صحيح، إزالته من القائمة
+		$_SESSION['csrf_tokens'] = array_diff($_SESSION['csrf_tokens'], [$submitted_token]);
+
+		// تحديث الجلسة بعد التعديل
+		// session_write_close();
+
+		// echo "Valid CSRF Token!";
 		return true;
 	} else {
+		// echo "Invalid or Reused CSRF Token!";
 		// ---
 		echo <<<HTML
 				<div class='alert alert-danger' role='alert'>
@@ -51,6 +68,16 @@ function verify_csrf_token()
 
 function generate_csrf_token()
 {
-	global $csrf_token;
-	return $csrf_token;
+	// if (session_status() === PHP_SESSION_NONE) {
+	// 	session_name("mdwikitoolforgeoauth");
+	// 	session_start();
+	// }
+
+	$token = bin2hex(random_bytes(32));
+	if (!isset($_SESSION['csrf_tokens'])) {
+		$_SESSION['csrf_tokens'] = [];
+	}
+	$_SESSION['csrf_tokens'][] = $token; // إضافة Token جديد إلى القائمة
+	return $token;
 }
+//---
