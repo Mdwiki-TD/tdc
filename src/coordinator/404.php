65489af8a7b39877af1fd7cfd54ac46782d03800
Ibrahem <ibrahem.al-radaei@outlook.com>
2025-10-02 06:30:25 +0300

    Update 404.php

--- src/coordinator/404.php
+++ src/coordinator/404.php
@@ -1,6 +1,5 @@
+<!--
 
-<!-- 
-    
 \$[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*\s*=\s*(['"])\s*<[\s\S]*?>\s*\1;
 
 (\$[a-zA-Z_\x7f-\xff][a-zA-Z0-9_\x7f-\xff]*\s*=\s*)(['"])(<\s*[\s\S]*?>\s*)\2;
@@ -13,13 +12,13 @@ $1<<<HTML\n$3\nHTML;
 
 $1$2<<<HTML$4$5$4HTML;
 -->
-<div class="content-wrapper pt-5">
-    <section class="content">
-      <div class="error-page">
-        <h3 class="headline text-warning">404</h3>
-        <div class="error-content">
-          <h4><i class="fas fa-exclamation-triangle text-warning"></i> Oops! Page not found.</h4>
-        </div>
-      </div>
-    </section>
-  </div>
+
+
+<div class="card border-danger">
+    <div class="card-header">
+        <h3 class="headline">404</h3>
+    </div>
+    <div class="card-body">
+        <h4><i class="fas fa-exclamation-triangle text-warning"></i> Page not found.</h4>
+    </div>
+</div>
