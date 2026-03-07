3f9ee17ca6215fb82663600512fd79960e7b881a
Ibrahem <ibrahem.al-radaei@outlook.com>
2026-03-08 02:44:26 +0300

    Update edit_user.php

--- src/coordinator/admin/Emails/edit_user.php
+++ src/coordinator/admin/Emails/edit_user.php
@@ -37,7 +37,7 @@ echo <<<HTML
     <div class='card-body'>
 HTML;
 
-function echo_form($user, $wiki, $project, $email, $user_id)
+function edit_user_echo_form($user, $wiki, $project, $email, $user_id)
 {
     //---
     $project_line = make_project_to_user($project);
@@ -105,7 +105,7 @@ function echo_form($user, $wiki, $project, $email, $user_id)
     HTML;
 }
 //---
-echo_form($user, $wiki, $project, $email, $user_id);
+edit_user_echo_form($user, $wiki, $project, $email, $user_id);
 //---
 echo <<<HTML
     </div>
