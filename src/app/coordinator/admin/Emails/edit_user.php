6fc1cf8f926c713b3bff89d7bd46edb377b3d4eb
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-09-25 01:49:09 +0300

    Update edit_user.php

--- src/app/coordinator/admin/Emails/edit_user.php
+++ src/app/coordinator/admin/Emails/edit_user.php
@@ -5,8 +5,8 @@ namespace App\Coordinator\Admin\Emails;
 
 use App\Coordinator\Admin\Common\AbstractController;
 use function App\Utils\Html\make_project_to_user;
+use function App\Utils\Html\make_form_input_col;
 use function App\APICalls\MdwikiSql\get_user_by_id;
-
 
 require_once __DIR__ . '/post.php';
 
@@ -40,14 +40,10 @@ class EditUserController extends AbstractController
      */
     public function handleRequest(): void
     {
-        $this->validateCoordinator();
-        $this->renderHeaderScripts();
-
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $this->handlePostRequest();
-        } else {
-            $this->renderFormCard();
-        }
+        $this->handlePopupRequest(
+            fn() => $this->renderFormCard(),
+            fn() => $this->handlePostRequest()
+        );
     }
     /**
      * Builds the user/email/wiki/project edit-or-add form markup.
@@ -62,20 +58,13 @@ class EditUserController extends AbstractController
 
         $projectLine = make_project_to_user($user_group);
 
-        $idRow = <<<HTML
-            <div class='col-md-3'>
-                <div class='input-group mb-3'>
-                    <div class='input-group-prepend'>
-                        <span class='input-group-text'>User id</span>
-                    </div>
-                    <input class='form-control' type='text' name='emails[1][user_id]' value='$userId' readonly/>
-                </div>
-            </div>
-        HTML;
+        $idRow = !empty($userId)
+            ? make_form_input_col('User id', 'emails[1][user_id]', $userId, 'col-md-3', '', true)
+            : '';
 
-        if (empty($userId)) {
-            $idRow = "";
-        }
+        $userCol = make_form_input_col('User', 'emails[1][username]', $user, 'col-md-3', 'required');
+        $emailCol = make_form_input_col('email', 'emails[1][email]', $email, 'col-md-3');
+        $wikiCol = make_form_input_col('wiki', 'emails[1][wiki]', $wiki, 'col-md-3');
 
         return <<<HTML
             <form action='index.php?ty=Emails/edit_user&nonav=120&user_id={$this->userId}' method="POST">
@@ -84,30 +73,9 @@ class EditUserController extends AbstractController
                 <div class='container'>
                     <div class='row'>
                         $idRow
-                        <div class='col-md-3'>
-                            <div class='input-group mb-3'>
-                                <div class='input-group-prepend'>
-                                    <span class='input-group-text'>User</span>
-                                </div>
-                                <input class='form-control' type='text' name='emails[1][username]' value='$user' required/>
-                            </div>
-                        </div>
-                        <div class='col-md-3'>
-                            <div class='input-group mb-3'>
-                                <div class='input-group-prepend'>
-                                    <span class='input-group-text'>email</span>
-                                </div>
-                                <input class='form-control' type='text' name='emails[1][email]' value='$email'/>
-                            </div>
-                        </div>
-                        <div class='col-md-3'>
-                            <div class='input-group mb-3'>
-                                <div class='input-group-prepend'>
-                                    <span class='input-group-text'>wiki</span>
-                                </div>
-                                <input class='form-control' type='text' name='emails[1][wiki]' value='$wiki'/>
-                            </div>
-                        </div>
+                        $userCol
+                        $emailCol
+                        $wikiCol
                         <div class='col-md-3'>
                             <div class='input-group mb-3'>
                                 <div class='input-group-prepend'>
