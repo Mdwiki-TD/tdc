78a3b1a642de9096791c4df79c669e512fc37323
ibrahem Qasim <ibrahem.al-radaei@outlook.com>
2026-09-25 03:00:42 +0300

    Update include.php

--- src/app/coordinator/include.php
+++ src/app/coordinator/include.php
@@ -4,4 +4,5 @@
 require_once __DIR__ . '/admin/common/AbstractPostHandler.php';
 require_once __DIR__ . '/admin/common/AbstractController.php';
 require_once __DIR__ . '/admin/common/AbstractEditController.php';
+require_once __DIR__ . '/admin/common/AbstractControllerNoPost.php';
 require_once __DIR__ . '/helpers/include.php';
